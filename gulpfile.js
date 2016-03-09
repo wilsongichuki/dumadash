52188b63964e9d51c1c930f06fc0a56f2fb53d6f
Wilson Gichuki <wilsongichuki@outlook.com>
2016-03-09 18:50:49 +0300

    Setting up environment

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,21 @@
 var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
     rename      = require('gulp-rename'),
-    browserSync = require('browser-sync'),
+    del         = require('del'),
+    clean       = require('gulp-clean'),
+    browserSync = require('browser-sync').create(),
     jasmine     = require('gulp-jasmine'),
-    reload      = function () {
-        browserSync.reload();
-    };
-
-
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'jasmine'], function() {
-
-    browserSync.init({
-        server: "./"
-    });
-
-    gulp.watch("./scss/*.scss", ['sass']);
-    gulp.watch(["./css/*.css", "./js/*.js", "./*.html", "./**/*.html"], reload);
-
-});
+    runSequence = require('run-sequence'),
+    git = require('gulp-git');
 
 // Sass stuff
 gulp.task('sass', function () {
-    gulp.src('./scss/*.scss')
+    gulp.src('./src/scss/*.scss')
         .pipe(sass({outputStyle: 'expanded'}))
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./dist/css'))
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(rename({extname: '.min.css'}))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('jasmine', function () {
@@ -44,5 +31,39 @@ gulp.task('jasmine', function () {
         }));
 });
 
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./dist"
+    });
+
+    gulp.watch("./src/scss/*.scss", ['sass']);
+    gulp.watch("./dist/**/*").on('change', browserSync.reload);
+
+});
+
+
 // Run
 gulp.task('default', ['serve']);
+
+
+
+// BUILDING FILES
+// Delete when copying files
+gulp.task('clean:dist', function(){
+    return del(['C:/Users/wilso_000/Desktop/dumadash-gh-pages/*.html', 'C:/Users/wilso_000/Desktop/dumadash-gh-pages/css', 'C:/Users/wilso_000/Desktop/dumadash-gh-pages/js', 'C:/Users/wilso_000/Desktop/dumadash-gh-pages/fonts', '!C:/Users/wilso_000/Desktop/dumadash-gh-pages/wireframes/**/*'], {force: true});
+});
+
+// Copy final files to dist folder
+gulp.task('copy:src', function(){
+    return gulp.src('dist/**/**/*', {base: 'dist'})
+        .pipe(gulp.dest('C:/Users/wilso_000/Desktop/dumadash-gh-pages'));
+});
+
+// Build the project by running the sequence
+gulp.task('build', function(){
+    runSequence('clean:dist', 'copy:src');
+});
+
+// git status && git add --all && git commit -a -m "minor fixes"
+// git pull && git push
